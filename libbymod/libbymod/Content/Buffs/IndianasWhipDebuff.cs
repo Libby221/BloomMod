ea75dc64168d1ce3bd4c27601dcfc106bb602732
Libby221 <149817526+Libby221@users.noreply.github.com>
2024-10-25 06:27:53 -0400

    Add files via upload

---
+++ libbymod/libbymod/Content/Buffs/IndianasWhipDebuff.cs
@@ -0,0 +1,40 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace libbymod.Content.Buffs
+{
+    public class IndianasWhipDebuff : ModBuff
+    {
+        public static readonly int TagDamage = 4;
+
+        public override void SetStaticDefaults()
+        {
+            // This allows the debuff to be inflicted on NPCs that would otherwise be immune to all debuffs.
+            // Other mods may check it for different purposes.
+            BuffID.Sets.IsATagBuff[Type] = true;
+        }
+    }
+
+
+
+    public class IndianasWhipDebuffNPC : GlobalNPC
+    {
+        public override void ModifyHitByProjectile(NPC npc, Projectile projectile, ref NPC.HitModifiers modifiers)
+        {
+            // Only player attacks should benefit from this buff, hence the NPC and trap checks.
+            if (projectile.npcProj || projectile.trap || !projectile.IsMinionOrSentryRelated)
+                return;
+
+
+            // SummonTagDamageMultiplier scales down tag damage for some specific minion and sentry projectiles for balance purposes.
+            var projTagMultiplier = ProjectileID.Sets.SummonTagDamageMultiplier[projectile.type];
+            if (npc.HasBuff<IndianasWhipDebuff>())
+            {
+                // Apply a flat bonus to every hit
+                modifiers.FlatBonusDamage += IndianasWhipDebuff.TagDamage * projTagMultiplier;
+            }
+
+        }
+    }
+}
---
+++ libbymod/libbymod/Content/Items/Weapons/WhiteWhale.cs
@@ -0,0 +1,34 @@
+﻿using libbymod.Content.Projectiles;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace libbymod.Content.Items.Weapons
+{
+    public class WhiteWhale : ModItem
+    {
+        public override void SetDefaults()
+        {
+            // This method right here is the backbone of what we're doing here; by using this method, we copy all of
+            // the meowmere's SetDefault stats (such as Item.melee and Item.shoot) on to our item, so we don't have to
+            // go into the source and copy the stats ourselves. It saves a lot of time and looks much cleaner; if you're
+            // going to copy the stats of an item, use CloneDefaults().
+
+            Item.CloneDefaults(ItemID.Harpoon);
+
+            // After CloneDefaults has been called, we can now modify the stats to our wishes, or keep them as they are.
+            // For the sake of example, let's swap the vanilla Meowmere projectile shot from our item for our own projectile by changing Item.shoot:
+
+            Item.shoot = ModContent.ProjectileType<WhiteWhaleProjectile>(); // Remember that we must use ProjectileType<>() since it is a modded projectile!
+                                                                              // Check out ExampleCloneProjectile to see how this projectile is different from the Vanilla Meowmere projectile.
+
+            // While we're at it, let's make our weapon's stats a bit stronger than the Meowmere, which can be done
+            // by using math on each given stat.
+
+            Item.damage = 45; // Makes this weapon's damage double the Meowmere's damage.
+            Item.shootSpeed *= 1.5f; // Makes this weapon's projectiles shoot 25% faster than the Meowmere's projectiles.
+        }
+    }
+}
